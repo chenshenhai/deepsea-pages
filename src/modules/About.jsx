14fc4bd2a92fc80559dd6ce8492e5be696d340f0
chenshenhai <cshenhai@gmail.com>
2018-07-12 00:16:10 +0800

    feat: update

--- src/modules/About.jsx
+++ src/modules/About.jsx
@@ -54,7 +54,7 @@ class About extends React.Component {
   }
   render() {
     return (
-      <div style={{'min-height':'720px'}}>
+      <div style={{'minHeight':'720px'}}>
         <div>
           <Tag color="magenta">JavaScript</Tag>
           <Tag color="red">Node.js</Tag>
